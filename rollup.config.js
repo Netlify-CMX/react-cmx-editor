b0b6b84c8a3f06101d1112eceaa0d945550d1722
mt <matheusluizn@gmail.com>
2023-01-15 01:13:40 -0300

    chore: setup css bundle and no react export

--- rollup.config.js
+++ rollup.config.js
@@ -2,7 +2,9 @@ import resolve from "@rollup/plugin-node-resolve";
 import commonjs from "@rollup/plugin-commonjs";
 import typescript from "@rollup/plugin-typescript";
 import dts from "rollup-plugin-dts";
-
+import postcss from "rollup-plugin-postcss";
+import peerDepsExternal from 'rollup-plugin-peer-deps-external';
+import terser from "@rollup/plugin-terser";
 import packageJson from "./package.json" assert { type: "json" };
 
 export default [
@@ -24,11 +26,15 @@ export default [
       resolve(),
       commonjs(),
       typescript({ tsconfig: "./tsconfig.json" }),
+      postcss(),
+      peerDepsExternal(),
+      terser()
     ],
   },
   {
     input: "dist/esm/types/index.d.ts",
     output: [{ file: "dist/index.d.ts", format: "esm" }],
     plugins: [dts()],
+    external: [/\.css$/]
   },
 ];
